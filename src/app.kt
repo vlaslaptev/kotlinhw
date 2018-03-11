1b9781c053bdde001a0d47762a6ad5b2c46a4f60
v.laptev <v.laptev@qiwi.com>
2018-03-11 23:05:12 +0300

    Http Rest hello world

--- src/app.kt
+++
@@ -1,7 +0,0 @@
-fun main(args: Array<String>) {
-    if (args.isEmpty()) {
-        println("Hello world")
-    } else {
-        Speaker(args[0]).salute()
-    }
-}
--- src/hello/Speaker.kt
+++ src/hello/Speaker.kt
@@ -1,3 +1,5 @@
+package hello
+
 class Speaker(private val name: String) {
     fun salute() {
         println("Hello world my name is " + name)
